cdf7f67351c038b57f671b0964917ec485a10bc2
julioRodrigoAcosta <jracosta1991@gmail.com>
2021-10-30 00:36:06 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Contdown from "./components/Contdown/Contdown";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="tarjeta-home">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Contdown/>
+        </div>
       </header>
     </div>
   );
---
+++ src/components/Contdown/Contdown.js
@@ -0,0 +1,69 @@
+import { useState, useRef, useEffect } from "react";
+import "./Contdown.css";
+
+const Contdown = () => {
+  const [timerDias, setTimerDias] = useState("00");
+  const [timerHoras, setTimerHoras] = useState("00");
+  const [timerMinutos, setTimerMinutos] = useState("00");
+  const [timerSegundos, setTimerSegundos] = useState("00");
+
+  let interval = useRef();
+
+  const starTimer = () => {
+    const countDownDate = new Date("Nov 05, 2021 17:00:00").getTime();
+    console.log(countDownDate);
+    interval = setInterval(() => {
+      const now = new Date().getTime();
+      const distance = countDownDate - now;
+      const dias = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const horas = Math.floor((distance % (1000 * 60 * 60 * 24) / (1000 * 60 * 60)));
+      const minutos = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const segundos = Math.floor((distance % (1000 * 60)) / 1000);
+
+      if (distance < 0) {
+      } else {
+        setTimerDias(dias);
+        setTimerHoras(horas);
+        setTimerMinutos(minutos);
+        setTimerSegundos(segundos);
+      }
+    }, 1000);
+  };
+
+  useEffect(() => {
+    starTimer();
+    return () => {
+      //eslint-disable-next-line
+      clearInterval(interval.current);
+    };
+  }, []);
+
+  return (
+    <div className="contdown-container">
+      <span className="c-black">Faltan</span>
+      <div className="temporizador-row">
+        <div className="temporizador-col col-margin-right">
+          <span className="temporizador c-black">{timerDias}</span>
+          <span className="temporizador-texto c-black">DIAS</span>
+        </div>
+        <div className="temporizador-col">
+          <span className="temporizador c-black">{timerHoras}</span>
+          <span className="temporizador-texto c-black">HORAS</span>
+        </div>
+        <span className="separador c-black">:</span>
+        <div className="temporizador-col">
+          <span className="temporizador c-black">{timerMinutos}</span>
+          <span className="temporizador-texto c-black">MINUTOS</span>
+        </div>
+        <span className="separador c-black">:</span>
+        <div className="temporizador-col">
+          <span className="temporizador c-black">{timerSegundos}</span>
+          <span className="temporizador-texto c-black">SEGUNDOS</span>
+        </div>
+      </div>
+      <span className="c-black">SE VA LLEJU</span>
+    </div>
+  );
+};
+
+export default Contdown;
